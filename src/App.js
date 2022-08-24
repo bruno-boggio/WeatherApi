555165291f60a7aad5ddd6390e0fbb67bd2c5733
bruno-boggio <brn_boggio@hotmail.com>
2022-08-24 16:33:50 -0300

    weather api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from 'axios'
+import ApiKey from '../src/Utils/Key'
+import { WiDayCloudyGusts } from "react-icons/wi";
+import { WiNightAltRain } from "react-icons/wi";
 
 function App() {
+
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&lang=pt&appid=${ApiKey}`
+  
+  const searchLocation = (e) => {
+    if (e.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">  
+        <h1>Previsão do tempo</h1>
+      <div>
+       <input 
+        onChange={e => setLocation(e.target.value)}
+        placeholder="Digite o lugar"
+        onKeyPress={searchLocation}
+        value={location}
+        type="text"
+       />
+       <div className='city'>{data.name}</div>
+    </div>
+        <div className='container'>
+        <WiDayCloudyGusts className="icon"/>
+         <div className='items'>
+         <div><p>Graus: {data.main ? <span>{data.main.temp.toFixed()}°</span> : null}</p></div>
+         <hr></hr>
+          <div><p>Máxima: {data.main ? <span>{data.main.temp_max.toFixed()}°</span> : null}</p></div>
+          <hr></hr>
+          <div><p>Mínima: {data.main ? <span>{data.main.temp_min.toFixed()}°</span> : null}</p></div>
+          <hr></hr>
+          <div><p>Sensação: {data.main ? <span>{data.main.feels_like.toFixed()}°</span> : null}</p></div>
+          <hr></hr>
+          <div><p>Umidade: {data.main ? <span>{data.main.humidity}%</span> : null}</p></div>
+          <hr></hr>
+          <div><p>Tempo: {data.weather? <span>{data.weather[0].description}</span> : null}</p></div>
+          <hr></hr>
+         </div>
+        </div> 
     </div>
   );
 }
---
+++ src/Utils/Key.js
@@ -0,0 +1,3 @@
+const ApiKey = "b39c81ba8c8e60c37d8e9b9f8f9e8571"
+
+export default ApiKey
